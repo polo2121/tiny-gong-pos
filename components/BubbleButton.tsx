1984e9da41c015128a3b9117c6dcb23deea7f87a
polo2121 <billykhant1357@gmail.com>
2025-12-08 13:32:40 +0700

    update BubbleButton

--- components/BubbleButton.tsx
+++ components/BubbleButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-const BubbleButton = ({ name: String }) => {
+const BubbleButton = () => {
   const buttonType = {
     sale: {
       name: "Daily Sale",
